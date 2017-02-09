3b82b5e13ff99c092240d2ff13507169ed59fb87
stevieray8450 <stevieray8450@STEPHENBONID14B>
2017-02-09 07:13:13 -0600

    Add project files.

---
+++ App1/Faculty.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows;
+using System.ComponentModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Faculty : Page
+    {
+        public Faculty()
+        {
+            this.InitializeComponent();
+            BL_PageContent.CreatedBy = "Created by: Steve Boniface";
+            courseFaculty.Text = "";
+            txtBoxFooter.Text = BL_PageContent.CreatedBy;
+
+            txtCourse.Text = "Click your class button below to display faculty information.";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // executes Course1 method from BL_PageContent. This loads all relevant data to course 1.
+            // this data is then used to fill the txtCourse, courseFaculty, aboutCourse, and coursePrereq text blocks
+            BL_PageContent.Course1();
+
+            txtCourse.Text = BL_PageContent.CourseID + "\n" + BL_PageContent.CourseCode + "\n" + BL_PageContent.CourseTitle;
+            courseFaculty.Text = BL_PageContent.FacultyMember;
+            aboutCourse.Text = BL_PageContent.AboutCourse;
+            coursePrereq.Text = BL_PageContent.CreditsPrereq;
+
+            // sending the correct image file path to the courseFacultyPic "placeholder" in the XAML file
+            courseFacultyPic.Source = new BitmapImage(new Uri(courseFacultyPic.BaseUri, "syedNabeel.png"));
+        }
+
+        private void course2Btn_Click(object sender, RoutedEventArgs e)
+        {
+            // executes Course2 method from BL_PageContent. This loads all relevant data to course 2.
+            // this data is then used to fill the txtCourse, courseFaculty, aboutCourse, and coursePrereq text blocks
+            BL_PageContent.Course2();
+
+            txtCourse.Text = BL_PageContent.CourseID + "\n" + BL_PageContent.CourseCode + "\n" + BL_PageContent.CourseTitle;
+            courseFaculty.Text = BL_PageContent.FacultyMember;
+            aboutCourse.Text = BL_PageContent.AboutCourse;
+            coursePrereq.Text = BL_PageContent.CreditsPrereq;
+
+            // sending the correct image file path to the courseFacultyPic "placeholder" in the XAML file
+            courseFacultyPic.Source = new BitmapImage(new Uri(courseFacultyPic.BaseUri, "cynthiaMoonhowler.png"));
+        }
+
+        private void course3Btn_Click(object sender, RoutedEventArgs e)
+        {
+            // executes Course3 method from BL_PageContent. This loads all relevant data to course 3.
+            // this data is then used to fill the txtCourse, courseFaculty, aboutCourse, and coursePrereq text blocks.
+            BL_PageContent.Course3();
+
+            txtCourse.Text = BL_PageContent.CourseID + "\n" + BL_PageContent.CourseCode + "\n" + BL_PageContent.CourseTitle;
+            courseFaculty.Text = BL_PageContent.FacultyMember;
+            aboutCourse.Text = BL_PageContent.AboutCourse;
+            coursePrereq.Text = BL_PageContent.CreditsPrereq;
+
+            // sending the correct image file path to the courseFacultyPic "placeholder" in the XAML file
+            courseFacultyPic.Source = new BitmapImage(new Uri(courseFacultyPic.BaseUri, "shadMcdillek.jpeg"));
+        }
+
+    }
+}
---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessCalc();
+        }
+
+        private void ProcessCalc()
+        {
+            Int32 Var1 = Convert.ToInt32(txtBoxInput1.Text) + Convert.ToInt32(txtBoxInput2.Text);
+            txtBoxDisplay.Text = Convert.ToString(Var1);
+        }
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessSubtract();
+        }
+
+        private void ProcessSubtract()
+        {
+            int var1Verify;
+            int var2Verify;
+            int result;
+
+            if (Int32.TryParse(txtBoxInput1.Text, out var1Verify) && (Int32.TryParse(txtBoxInput2.Text, out var2Verify)))
+            {
+                result = var1Verify - var2Verify;
+                txtBoxDisplay.Text = Convert.ToString(result);
+            }
+            else
+            {
+                txtBoxDisplay.Text = "Invalid integer used. Please use numerals 0-9 only";
+            }
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            int var1Verify;
+            int var2Verify;
+            int result;
+
+            if (Int32.TryParse(txtBoxInput1.Text, out var1Verify) && (Int32.TryParse(txtBoxInput2.Text, out var2Verify)))
+            {
+                result = var1Verify * var2Verify;
+                txtBoxDisplay.Text = Convert.ToString(result);
+            }
+            else
+            {
+                txtBoxDisplay.Text = "Invalid integer used. Please use numerals 0-9 only";
+            }
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            double var1Verify;
+            double var2Verify;
+            double result;
+
+            if (Double.TryParse(txtBoxInput1.Text, out var1Verify) && (Double.TryParse(txtBoxInput2.Text, out var2Verify)))
+            {
+                result = var1Verify / var2Verify;
+                txtBoxDisplay.Text = Convert.ToString(result);
+            }
+            else
+            {
+                txtBoxDisplay.Text = "Invalid integer used. Please use numerals 0-9 only";
+            }
+        }
+
+        async private void btnCourse1_Click(object sender, RoutedEventArgs e)
+        {
+            BL_PageContent.Course1();
+            txtBoxCourse.Text = BL_PageContent.VarOutput + "\n" + BL_PageContent.CreditsPrereq;
+        }
+
+        async private void btnCourse2_Click(object sender, RoutedEventArgs e)
+        {
+            BL_PageContent.Course2();
+            txtBoxCourse.Text = BL_PageContent.VarOutput + "\n" + BL_PageContent.CreditsPrereq;
+        }
+
+        async void btnCourse3_Click(object sender, RoutedEventArgs e)
+        {
+            BL_PageContent.Course3();
+            txtBoxCourse.Text = BL_PageContent.VarOutput + "\n" + BL_PageContent.CreditsPrereq;
+        }
+
+        public void hypLinkPage2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Faculty));
+        }
+    }
+}
